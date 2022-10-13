5f04309dba907d6b91678d812de61f71e9ddf4c3
Mark Shliakhov <valmarksh@gmail.com>
2022-10-13 20:23:13 +0300

    Squashed commit of the following:
    commit d603dff2660f078fbf8b26b9ba37cd33ac745f58
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 20:04:44 2022 +0300
    
        1
    
    commit 91a0a30e5fb4e1266e2c048d288aed13d9538452
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 20:04:33 2022 +0300
    
        1
    
    commit 2e73890b474c3a5c23e986e8fde32a3917d4a899
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 19:54:36 2022 +0300
    
        to fix conflict
    
    commit ccc0edea72e7b39d665c683f9a253c69f387bea0
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 19:36:52 2022 +0300
    
        to fix conflict
    
    commit 2a6b175d02e706ff0b071953428783484ba1852e
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 15:04:24 2022 +0300
    
        modal pictures
    
    commit 2335ded39cf3d94176eded0081e1b9a1c66a2610
    Merge: 61adc1b 6f1f78a
    Author: Arinagar <113587563+Arinagar@users.noreply.github.com>
    Date:   Thu Oct 13 14:10:25 2022 +0300
    
        Merge pull request #23 from Arinagar/anton-branch
    
        Anton branch
    
    commit 6f1f78a02c5c45b9a792837f21b9de98eaf28a99
    Merge: dcee79b 61adc1b
    Author: AntonVitulskyi <vitulskiyanton@gmail.com>
    Date:   Thu Oct 13 14:00:09 2022 +0300
    
        Merge branch 'main' into anton-branch
    
    commit dcee79b39627159a5af7c5f76d7c060ae23d0884
    Author: AntonVitulskyi <vitulskiyanton@gmail.com>
    Date:   Thu Oct 13 13:58:42 2022 +0300
    
        added modal for "Buy Now" adaptive
    
    commit 61adc1b7224d531a477f44e1878c2a748d3c1f8d
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 12:06:04 2022 +0300
    
        contacts modal
    
    commit 61a3a42e8690e918cc4c2ece425b5ce86decbe35
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 11:08:24 2022 +0300
    
        mobile_menu closes after click on link
    
    commit a4c066a73f80e4b4a03faab8b7122bdf4ffa921d
    Author: Mark Shliakhov <valmarksh@gmail.com>
    Date:   Thu Oct 13 11:02:23 2022 +0300
    
        finish cow modal

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,8 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
- 
+  const links = document.querySelectorAll('.menu_link');
+
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -15,9 +16,9 @@
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
-
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  links.forEach(link => link.addEventListener('click', toggleMenu));
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
@@ -27,4 +28,3 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
-
